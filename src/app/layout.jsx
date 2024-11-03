1511011e7fb32e3ef0542d52b40ae0bceeaaaf2e
Glory-Mmachi <Mglory360@gmail.com>
2024-11-03 20:39:42 +0100

    Added Fvicon and description

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,7 +3,7 @@ import "./globals.css"; // Importing global styles
 // Defining metadata for the page
 export const metadata = {
   title: "Monito Hub",
-  description: "A hub for pets",
+  description: "Monito Hub: Your go-to destination for everything pets! Discover a vibrant community, expert resources, and essential services that enhance the lives of pets and their owners. Join us to connect, share, and celebrate the joy of pet companionship",
 };
 
 // Defining the RootLayout function component
